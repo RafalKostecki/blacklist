7fbd2697cc138eca316892ec710d60c8b1db0de0
Rafał Kostecki <edward-12@o2.pl>
2023-12-29 18:37:20 +0100

    Add pagination and sort to the table

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -4,11 +4,23 @@ import { Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Sort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatTableModule, DatePipe, HttpClientModule],
+  imports: [
+    MatTableModule,
+    DatePipe,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -19,11 +31,17 @@ export class ListComponent implements OnInit {
     'surname',
     'content',
     'city',
+    'verified',
   ];
 
-  public loading: boolean = false;
+  public isLoading: boolean = false;
   public dataSource: any[] = [];
   public error: any = null;
+  public currentPage = 0;
+  public pageSizeOptions = [1, 3, 5, 10];
+  public currentPageSize = 3;
+  public totalElements = 1;
+  private sortParam = 'name,asc';
 
   constructor(private router: Router, private http: HttpClient) {}
 
@@ -32,23 +50,37 @@ export class ListComponent implements OnInit {
   }
 
   private fetchBlacklistData() {
-    this.loading = true;
+    this.isLoading = true;
 
-    this.http.get('http://localhost:8080/api/blacklisted').subscribe({
-      next: (data: any) => (this.dataSource = data),
-      error: (error) => (this.error = error),
-      complete: () => (this.loading = false),
-    });
+    this.http
+      .get(
+        `http://localhost:8080/api/blacklisted?page=${this.currentPage}&size=${this.currentPageSize}&sort=${this.sortParam}`
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log('data', data);
+          this.dataSource = data?.content;
+          this.totalElements = data?.totalElements;
+        },
+        error: (error) => (this.error = error),
+        complete: () => (this.isLoading = false),
+      });
   }
 
   public openRecord(element: any) {
-    console.log('openRecord called');
-    console.log('element', element);
-
-    // Dodatkowe logi do debugowania
-    console.log('Navigating to:', ['/blacklisted', element.id]);
-
-    // Prosta nawigacja do ścieżki '/blacklisted/:id'
     this.router.navigate(['/blacklisted', element.id]);
+  }
+
+  public onPageChange($event: PageEvent) {
+    this.currentPage = $event.pageIndex;
+    this.currentPageSize = $event.pageSize;
+
+    this.fetchBlacklistData();
+  }
+
+  public announceSortChange($event: Sort) {
+    this.sortParam = `${$event.active},${$event.direction}`;
+
+    this.fetchBlacklistData();
   }
 }
